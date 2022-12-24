ed9366eee13528c030e1ca803f77f4618ef4b079
ElenaV5 <elenavnev@gmail.com>
2022-12-24 17:44:31 +0500

    Cats ArrayList example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,7 +1,9 @@
 package ru.synergy;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,5 +19,11 @@ public class Main {
         System.out.println(Arrays.toString(cats));
         //[Cat{name='Tomas'}, Cat{name='Бегемот'}, Cat{name='Дымка'}]
 
+        ArrayList<Cat> catsList = new ArrayList();
+        for(Cat cat : cats){
+            catsList.add(cat);
+        }
+        catsList.add(new Cat("Гипопотам")); //добавляется только в ArrayList
+        System.out.println(catsList.toString());
     }
 }
